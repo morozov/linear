a1f5e237897e014a7fa3d753f3d47ddb5b8608b8
Sergei Morozov <morozov@tut.by>
2020-06-10 07:50:48 -0700

    Reformat code

--- src/loader.asm
+++ src/loader.asm
@@ -1,47 +1,57 @@
-LD   IX,$4000
-PUSH IX
-LD   DE,$0020
-SUB  A
-DEC  A
-SCF
-DI
-INC  D
-EX   AF,AF'
-DEC  D
-CALL $056A
-POP  DE
-LD   B,$03
-PUSH BC
-LD   B,$08
-PUSH BC
-PUSH DE
-PUSH BC
-JR   K1
-;
-L1     PUSH BC
-LD   B,$08
-L2     PUSH BC
-PUSH IX
-LD   B,$08
-L3     PUSH BC
-LD   DE,$0020
-CALL $05A9
-K1     POP  BC
-LD   DE,224
-ADD  IX,DE
-DJNZ L3
-POP  IX
-POP  BC
-LD   DE,$0020
-ADD  IX,DE
-DJNZ L2
-POP  BC
-LD   DE,1792
-ADD  IX,DE
-DJNZ L1
-LD   IX,$5800
-LD   DE,$0300
-CALL $05A9
-EI
-RET  C
-JP   $0806
+LD_LOOP  equ $05A9 ; https://skoolkid.github.io/rom/asm/0556.html#05A9
+LD_BREAK equ $056B ; https://skoolkid.github.io/rom/asm/0556.html#056B
+
+    ld      ix, $4000
+    push    ix
+    ld      de, $0020
+    sub     a
+    dec     a
+    scf
+    di
+    inc     d
+    ex      af, af'
+    dec     d
+    call    LD_BREAK-1
+    pop     de
+    ld      b, $03
+    push    bc
+    ld      b, $08
+    push    bc
+    push    de
+    push    bc
+    jr      k1
+
+l1:
+    push    bc
+    ld      b, $08
+
+l2:
+    push    bc
+    push    ix
+    ld      b, $08
+
+l3:
+    push    bc
+    ld      de, $0020
+    call    LD_LOOP
+
+k1:
+    pop     bc
+    ld      de, $e0
+    add     ix, de
+    djnz    l3
+    pop     ix
+    pop     bc
+    ld      de, $0020
+    add     ix, de
+    djnz    l2
+    pop     bc
+    ld      de, $0700
+    add     ix, de
+    djnz    l1
+    ld      ix, $5800
+    ld      de, $0300
+    call    LD_LOOP
+    ei
+    ret     c
+    jp      $0806
