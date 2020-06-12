6a01091405192dda75370144ce1c484c1bf58f6c
Sergei Morozov <morozov@tut.by>
2020-06-12 09:05:24 -0700

    Switch to another implementation of the loader

--- src/loader.asm
+++ src/loader.asm
@@ -1,59 +1,38 @@
 LD_LOOP  equ $05A9 ; https://skoolkid.github.io/rom/asm/0556.html#05A9
 LD_BREAK equ $056B ; https://skoolkid.github.io/rom/asm/0556.html#056B
 
-    xor     a
-    ld      c, a
-    ld      ix, $4000
-    push    ix
-    ld      de, $0020
-    sub     a
-    dec     a
-    scf
+; (c) by Kabanov Alexandr
+; ZX-Review 1996 #07-08
     di
-    inc     d
-    ex      af, af'
-    dec     d
-    call    LD_BREAK-1
-    pop     de
-    ld      b, $03
-    push    bc
-    ld      b, $08
-    push    bc
-    push    de
-    push    bc
-    jr      k1
-
-l1:
-    push    bc
-    ld      b, $08
-
-l2:
-    push    bc
-    push    ix
-    ld      b, $08
-
-l3:
-    push    bc
-    ld      de, $0020
-    call    LD_LOOP
-
-k1:
-    pop     bc
-    ld      de, $e0
-    add     ix, de
-    djnz    l3
-    pop     ix
-    pop     bc
-    ld      de, $0020
-    add     ix, de
-    djnz    l2
-    pop     bc
-    ld      de, $0700
-    add     ix, de
-    djnz    l1
-    ld      ix, $5800
-    ld      de, $0300
-    call    LD_LOOP
+    xor  a
+    sub  $01
+    ex   af, af'
+    ld   c, $00
+    ld   ix, $4000
+    ld   de, $0020
+    call LD_BREAK-1
+    ld   b, $bf
+loop:
+    push bc
+    ld   h, c
+    ld   bc, $e0
+    add  ix, bc
+    ld   a, $07
+    and  ixh
+    jr   nz, prod
+    ld   a, ixl
+    add  a, $20
+    ld   ixl, a
+    jr   c, prod
+    ld   bc, $f800
+    add  ix, bc
+prod:
+    ld   c, h
+    ld   e, $20
+    call LD_LOOP
+    pop  bc
+    djnz loop
+    ld   d, $03
+    call LD_LOOP
     ei
-    ret     c
-    jp      $0806
+    ret
